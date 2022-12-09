061513cd7d1b9b3fbde13df0348d69e5af82f775
lakirasiza <katreekc@gmail.com>
2022-12-09 21:33:47 +0300

    js

---
+++ index.js
@@ -0,0 +1,22 @@
+// let fruits=['apple','lemon','banana'] //massive
+// console.log(fruits.length) //длинна массива
+// console.log(fruits.at(0)) //вывод элемента массива через .at
+// fruits.unshift('melon')//добавление элемента в начало массива через .unshift 
+// console.log(fruits) //вывод массива
+// console.log(fruits.length)//длинна массива
+// console.log(fruits.pop()) //вызывает последний эллемент массива и удаляет его 
+// console.log(fruits)//вывод массива
+// fruits.push('banana') //вставляет в конец массива элемент
+// console.log(fruits)
+// let a=fruits.shift(0) // вызывает первый эллемент массива и удаляет его
+// console.log(a)
+// console.log(fruits)
+// for (fruits of fruits){ //
+//     console.log(fruits) // вывод массива в столбик через for
+// }                       //
+// let arr=[1,2,3,4,5,6,7,8,9]
+// arr.length=4
+// console.log(arr[5])
+// arr.length=9
+// console.log(arr)
+
